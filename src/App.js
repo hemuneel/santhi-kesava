10af89ae57fe1b6a764726b1cf485e0daf4bd633
hemanthrec <hemanthsai@reckonsys.com>
2021-05-24 20:35:53 +0530

    Added Sign-in Page and reusable component custom button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header.component'
 
@@ -11,6 +12,7 @@ function App() {
         <switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shops' component={ShopPage}/>
+          <Route path='/signin' component={SignInAndSignUpPage}/>
         </switch>
     </div>
   );
