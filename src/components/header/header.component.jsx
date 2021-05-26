61ce089a70ea7ac77d7bfc54775b6b2dad8a9913
hemuneel <hemanthsai.satti@gmail.com>
2021-05-26 16:52:43 +0530

    Updated signin,signin with google and signout functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,6 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils'
 
 import './header.styles.scss';
-import reactDom from 'react-dom';
 
 const Header = ({ currentUser }) => (
     <div className = 'header'>
--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -1,55 +1,65 @@
-import React from 'react';
-import SignInAndSignUpPage from '../../pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import FormInput from '../form-input/form-input.components';
-import CustomButton from '../custom-button/custom-button.component';
-import './sign-in.styles.scss';
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import React from "react";
+import SignInAndSignUpPage from "../../pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import FormInput from "../form-input/form-input.components";
+import CustomButton from "../custom-button/custom-button.component";
+import "./sign-in.styles.scss";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
 
-class SignIn extends React.Component{
-    constructor(props){
-        super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
 
-        this.state={
-            email: '',
-            password: ''
-        }
-    }
+  handleSubmit = (event) => {
+    event.preventDefault();
 
-    handleSubmit = event => {
-        event.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
 
-        this.setState({email:'',password:''})
-    }
+  handleChange = (event) => {
+    const { value, name } = event.target;
 
-    handleChange = event => {
-        const { value,name } = event.target;
+    this.setState({ [name]: value });
+  };
 
-        this.setState({[name]:value})
-    }
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
 
-    render(){
-        return(
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
-
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput name = "email" type="email" value={this.state.email} label='Email' handleChange={this.handleChange} required/>
-                    <FormInput name = "password" type="password" value={this.state.email}  label='Password' handleChange={this.handleChange} required/>
-                    <div className='buttons'>
-                    
-                    <CustomButton type='submit'>SIGN IN</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                        {' '}
-                        SIGN IN WITH GOOGLE{' '}
-                    </CustomButton>
-                    </div>
-
-                </form>
-            </div>
-        )
-    }
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            value={this.state.email}
+            label="Email"
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.email}
+            label="Password"
+            handleChange={this.handleChange}
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit">SIGN IN</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              SIGN IN WITH GOOGLE
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
